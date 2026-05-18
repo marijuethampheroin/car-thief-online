e99a87cc19cbc0752feafa3819536693636464b0
marijuethampheroin <marijuethampheroin@email.com>
2026-05-17 22:28:44 -0500

    fix: inline firebase config to fix auth on GitHub Pages

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+﻿const firebaseConfig = {
+  apiKey: "AIzaSyDsh_Wpfeed0gQU6m1aZgHENhMb9tBYi4c",
+  authDomain: "car-thief-online.firebaseapp.com",
+  projectId: "car-thief-online",
+  storageBucket: "car-thief-online.firebasestorage.app",
+  messagingSenderId: "942239496002",
+  appId: "1:942239496002:web:e387b44b309372905dbadb"
+};
